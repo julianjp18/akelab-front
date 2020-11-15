5ee4fea317d649d34ad2f7c81c0d1a9d6126ee35
Julian Perez <julianperez.developer@gmail.com>
2020-11-15 06:56:13 -0500

    deleting others folders, only with build files

--- src/helpers/form/NumberInput.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { InputNumber } from "antd";
-
-const NumberInput = ({ value, onChange }) => (
-  <InputNumber min={1} value={value} onChange={onChange} />
-);
-
-export default NumberInput;
--- src/utils/helperFunctions.js
+++
@@ -1,5 +0,0 @@
-const numOnly = (value) => value && `${value}`.replace(/[^0-9.]/g, '');
-
-export {
-    numOnly
-};
